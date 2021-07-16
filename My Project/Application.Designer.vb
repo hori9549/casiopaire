a78c4af061b0c9750a85238d9304f3b1d27f641e
hori9549 <hori9549@gmail.com>
2021-07-17 04:51:31 +0900

    test

--- My Project/Application.Designer.vb
+++ My Project/Application.Designer.vb
@@ -32,7 +32,7 @@ Namespace My
         
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()>  _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.カシオペイア.frmメニュー
+            Me.MainForm = Global.カシオペイア.Form1
         End Sub
     End Class
 End Namespace
